08ce73bbdbae5cf41bc4d4329591677067e507e5
TombStone-dev <ZebeckN26@chcstudent.com>
2025-09-09 09:29:32 -0400

    commit

--- variables.py
+++ variables.py
@@ -8,3 +8,19 @@ print(num*x)
 print(x/num)
 sum = num + x
 print(sum)
+x = 5
+y = 10 
+z = 0
+z = x + y
+print(z)
+z = z/2
+print(z)
+name = "Nick Zebeck"
+occupation = "Student"
+print(name)
+print(occupation)
+print(f"My name is {name} and my occupation is {occupation}")
+first = "Calvert"
+Second = "Hall"
+School = first + Second
+print(School)
