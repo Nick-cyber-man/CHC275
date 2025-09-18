ff3992292614250afa7affa6c944295fda510e6f
TombStone-dev <230173756+Nick-cyber-man@users.noreply.github.com>
2025-09-18 10:16:06 -0400

    comm

---
+++ Problem sets/Fish.py
@@ -0,0 +1,7 @@
+carnivorous = int("carnivorous")
+saltwater = int("Saltwater")
+community = int("community")
+fish = input("what kind of fish do you have ")
+if fish == carnivorous
+    input(what)
+elif fish == saltwater 
