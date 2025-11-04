b7a1ae44a9f51788080879363adc3bcd20547af6
TombStone-dev <230173756+Nick-cyber-man@users.noreply.github.com>
2025-11-04 10:11:40 -0500

    commit

--- fileio.py
+++ fileio.py
@@ -13,3 +13,36 @@ for line in buffer:
     grades.append(line[1])
 print(names)
 print(grades)
+
+#file = open("name.txt","w")
+#line = "hello world"
+#file.write(line)
+#file.close()
+
+file = open("names.txt", "w")
+for i in range(len(names)):
+    line = f"{names[i]},{grades[i]}\n"
+    buffer.append(line)
+    
+buffer[-1] = buffer[-1].strip()
+file.writelines(buffer)
+file.closed()
+
+try:
+    name = input("enter your name: ").strip()
+    grade = input("enter yourt grade: ")
+    grade = int(grade)
+except Exception as e:
+    print(e)
+else:
+    file = open("names.txt", "a")
+    line = f"\n{name},{grade}"
+    file.write(line)
+    file.close()
+    
+date = "Nov42025"
+file = open(date,"w")
+avg = sum(grades)/len(grades)
+line = f"the average GPA on {date} is {avg}"
+file.write(line)
+file.close()
