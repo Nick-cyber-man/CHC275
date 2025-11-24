b86ae343e897352fac8912adbe313cdf5c76a779
TombStone-dev <grantjackson2009@outlook.com>
2025-11-24 09:09:59 -0500

    Update Grocery.py

--- Lab4/Grocery.py
+++ Lab4/Grocery.py
@@ -1,8 +1,6 @@
 Food = []
 Prices = []
 total = 0
-cart = []
-quanities = []
 
 file = open("food.txt" , "r")
 buffer = file.readlines()
@@ -12,21 +10,38 @@ for line in buffer:
     Food.append(line[0])
     line[1] = float(line[1])
     Prices.append(line[1])
+check = False
+while check == False:
+    print("Welcome to the Grocery store check-out")
+    print("1. add to cart")
+    print("2. remove items")
+    print("3. check-out")
+    option = input("what service would you like? ")
 
-print("Welcome to the Grocery store check-out")
-print("1. add to cart")
-print("2. remove items")
-print("3. check-out")
-option = input("what service would you like? ")
+    if option == "1":
+        print(Food)
+        print(Prices)
+        try:
+            x = input("what value corresponding to food/price would you like to add to your cart? ")
+            x = int(x)
+            y = input(f"How many {Food[x]} would you like? ")
+            y = int(y)
+            total = total + y * Prices[x]
+        except Exception as e:
+            print(e)
 
-if option == "1":
-    print(Food)
-    print(Prices)
-    option = input("what value corresponding to food/price would you like to add to your cart? ")
-    try:
-        option = int(option)
-        Quanity = input(f"How many {Food[option]} would you like? ")
-    except Exception as e:
-        print(e)
+    if option == "2":
+        try:
+            x = input("what value corresponding to food/price would you like to remove to your cart? ")
+            x = int(x)
+            y = input(f"How many {Food[x]} would you like? ")
+            y = int(y)
+            total = total - y * Prices[x]
+        except Exception as e:
+            print(e)
 
-    
+    if option == "3":
+        tax = total * 0.06
+        cart = total + tax
+        print(f"The value of your purchase is: {cart}")
+        check = True
