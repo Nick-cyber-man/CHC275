9fa3671baa4bc7dd96a2301a6d2767335976080e
TombStone-dev <grantjackson2009@outlook.com>
2025-12-04 11:15:21 -0500

    c

--- Semester project/casino.py
+++ Semester project/casino.py
@@ -1,8 +1,31 @@
-print("1. New Player")
-print("2. Returning Player")
-Option = input("Are you a new or returning memeber?")
-
 file = open("Accounts.txt","r")
 buffer = file.readlines()
 
-print(buffer)
+print("1. New Player")
+print("2. Returning Player")
+option = input("Are you a new or returning memeber?")
+
+check = False
+if option == "1":
+    Accountnames = []
+    Passwords = []
+    Balances = []
+    
+    
+    accountname = input("What would you like your account name to be?")
+    password = input("What would you like the password for the account to be?(has to be 5 numbers in length)")
+    Balance = input("How much money do you want in your account?")
+    
+    if accountname.isalpha() and password.isnumeric() and len(password) < 5:
+        Accountnames.append(accountname)
+        Passwords.append(password)
+        Balances.append(Balance)
+
+    else:
+        print("This password is too long and or short, make sure it is 5 numbers long")
+
+    check = True
+
+    
+
+    
