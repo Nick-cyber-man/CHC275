f3e16a653b082381aa5b39fd9f767f249b1acb2d
TombStone-dev <grantjackson2009@outlook.com>
2025-12-05 09:31:26 -0500

    c

--- Semester project/casino.py
+++ Semester project/casino.py
@@ -1,5 +1,16 @@
 file = open("Accounts.txt","r")
 buffer = file.readlines()
+Accountnames = []
+Passwords = []
+Balances = []
+for line in buffer:
+    line = line.strip()
+    line = line.split(",")
+    Accountnames.append(line[0])
+    Passwords.append(line[1])
+    line[2] = int(line[2]) 
+    Balances.append(line[2]) 
+file.close()     
 
 print("1. New Player")
 print("2. Returning Player")
@@ -7,16 +18,13 @@ option = input("Are you a new or returning memeber?")
 
 check = False
 if option == "1":
-    Accountnames = []
-    Passwords = []
-    Balances = []
-    
+
     
     accountname = input("What would you like your account name to be?")
     password = input("What would you like the password for the account to be?(has to be 5 numbers in length)")
     Balance = input("How much money do you want in your account?")
     
-    if accountname.isalpha() and password.isnumeric() and len(password) < 5:
+    if accountname.isalpha() and password.isnumeric() and len(password) == 5:
         Accountnames.append(accountname)
         Passwords.append(password)
         Balances.append(Balance)
@@ -24,8 +32,19 @@ if option == "1":
     else:
         print("This password is too long and or short, make sure it is 5 numbers long")
 
-    check = True
+print (f"Your Account name is: {accountname}, The password to your account is:{password}, Your starting balance is: {Balance}")
+
+ 
+if option == "2":
+    pickedaccount = input("What account would you like to use")
+    pickedpassword = input("What is the password for this account")
+    
+    if pickedaccount.isalpha() and pickedpassword.isnumeric() and len(password) == 5:
+        pass
+    else:
+        print("This account name/ password is wrong or doesnt exist")
+        
+        check = True
 
     
 
-    
