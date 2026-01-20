fb4f56f49142a24d9ae06966ce61a592f6b41eb5
TombStone-dev <grantjackson2009@outlook.com>
2026-01-20 10:11:52 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,29 @@
+def foo():
+    print("bar")
+    
+foo ()
+
+def add5(num):
+    num = num + 5
+    print(num)
+    
+add5(8)
+add5(4)
+
+mynum = 10
+add5(mynum)
+
+def multiply(num1,num2):
+    product = num1 * num2
+    print(product)
+    
+multiply(3,5)
+multiply(4,5)
+multiply(4,"a")
+
+def add(num1,num2):
+    sum=num1+num2
+    print(sum)
+    
+add(2,5)
+add("hello","world")
