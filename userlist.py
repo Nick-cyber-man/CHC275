cb8172684820736ca0babd70db60e423debd7235
TombStone-dev <grantjackson2009@outlook.com>
2026-02-06 08:50:13 -0500

    Create userlist.py

---
+++ userlist.py
@@ -0,0 +1,23 @@
+    num1 = num1 + 10
+x = 5
+update(x)
+print(x)
+print(x)
+
+mylist = [1,2,3,4]
+print(mylist)
+def update2(num5):
+    num5.append(5)
+    
+update2(mylist)
+print(mylist)
+
+
+def foo(x):
+    factor = 0.2
+    print(factor)
+    print(x*factor)
+
+foo(5)
+print(factor)
+print(100 * factor)
