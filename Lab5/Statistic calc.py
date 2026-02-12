f7f2de41cee3a1f9fc685f3d2299235523b9acc2
TombStone-dev <grantjackson2009@outlook.com>
2026-02-12 10:12:42 -0500

    Update Statistic calc.py

--- Lab5/Statistic calc.py
+++ Lab5/Statistic calc.py
@@ -1,5 +1,5 @@
 import math
-import test
+
 
 
 def getList():
@@ -23,21 +23,74 @@ def printMenu():
     print("4.Median")
     print("5. Standard Deviation")
     print("6. Clear list")
-    option = input("What statistic would you like to do on the list? ")
-    return option
+
 
 
 def getMin(userList):
-    pass
+    min = userList[0]
+    for num in userList:
+        if min > num:
+            min=num
+    return min
 
+def getmean(userList):
+    sum=0
+    for num in userList:
+        sum=num+sum
+    mean=sum/len(userList)
+    return mean
+        
+def getmedian(userList):
+    userList = sorted(userList)
+    y = len(list)
 
+    if y % 2 == 1:
+        mid=y/2
+        return list[mid]
+    else:
+        mid = (y // 2) - 1
+        mid2 = y // 2
+        z=(list[mid]+ list[mid2])/2
+        return z
 
+def getmax(userList):
+    max = userList[0]
+    for num in userList:
+        if max < num:
+            max=num
+    return max
+
+def getSTdDev(userList):
+    sse = 0
+    mean = getmean(userList)
+    for num in userList:
+        sse=sse+(num-mean)**2
+    sse=sse/len(userList)
+    return math.sqrt(sse)
 
 def main():
-    test.foo()
-    print("Welcome to the Statistics Calculator")
-    mynums= getList()
-    print(mynums)
+    enter=getList()
+    check = False
+    while check == False:
+        printMenu()
+        option=input("what function would you like to do? type quit to quit")
+        if option == "1":
+            ans=getMin(enter)
+            print(f"The minimum is {ans}")
+        if option == "2":
+            ans = getmax(enter)
+            print(f"the maximum is {ans}")
+        if option == "3":
+            ans = getmedian(enter)
+            print(f"The median is {ans}")
+        if option == "4":
+            ans = getmean(enter)
+            print(f"The mean is {ans}")
+        if option == "5":
+            ans = getSTdDev(enter)
+            print(f"The Standard deviation is {ans}")
+        if option =="quit":
+            check = True
 
 
 if __name__ == "__main__":
