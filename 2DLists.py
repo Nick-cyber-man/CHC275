5580a1dc3fad85371a14804998680c85008d0750
TombStone-dev <grantjackson2009@outlook.com>
2026-02-19 10:14:15 -0500

    Create 2DLists.py

---
+++ 2DLists.py
@@ -0,0 +1,40 @@
+grid = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],    
+]
+
+print(grid[0][0])
+
+
+grid2 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+
+print(grid2[2][2])
+
+for row in grid2:
+    for num in row:
+        print(num)
+        
+
+for i in range(len(grid2)):
+    for j in range(len(grid2[0])):
+        print(f"grid2{i},{j}) = {grid2[i][j]}", end = " ")
+    print()
+    
+    
+    
+    
+board = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+
+def printboard(board):
+    for i in range(len)
+    print("--------")
+    
