21de890d08bf75080308ddf51323faae94e226e7
TombStone-dev <grantjackson2009@outlook.com>
2026-03-17 09:30:32 -0400

    Update dictinaries.py

--- Lab6/dictinaries.py
+++ Lab6/dictinaries.py
@@ -1,48 +1,23 @@
 def getStudent(directory, student):
-    student = {}
-    student1 = {}
-    student2 = {[student]:10}
-    pass
+    return directory[student]
+
 
 def getStudentGrades(directory, student):
-    pass
+    return directory[student]["grades"]
+
 
 def getStudentGradeLevel(directory,student):
-    """
-        Function Name: getStudentGradeLevel
-        Parameters:
-            Directory <dict> : Student Directory that is specified in the main() function
-            student <String> : String that corresponds to the student name
-            Return Type:  integer corresponding to student's grade level
-        Description:
-            A Function that returns a Dictionary of the student's gradebook at dictionary[student]
-    """
-    pass
+    return directory[student]["gradelevel"]
 
 def getStudentEmail(directory,student):
-    """
-        Function Name: getStudentGradeLevel
-        Parameters:
-            Directory <dict> : Student Directory that is specified in the main() function
-            student <String> : String that corresponds to the student name
-            Return Type:  string corresponding to student's email
-        Description:
-            A Function that returns a string of the student's email at dictionary[student]
-    """
-    pass
+    return directory[student]["studentemail"]
+
 
 def getStudentsByGradeLevel(directory, gradelevel):
-    """
-        Function Name: getStudentsbyGradeLevel
-        Parameters:
-            Directory <dict> : Student Directory that is specified in the main() function
-            gradelevel <int> : integer corresponding to the grade level
-            Return Type:  none
-        Description:
-            procedure that prints out all of the students of a corresponding grade level.
-    """
-    pass
-
+    for student in directory:
+        if directory[student]["gradelevel"] == gradelevel:
+            print(directory[student])
+    
 def addStudent(directory):
     student = {}
     name = input("What is the name of the new student?").strip()
@@ -60,42 +35,20 @@ def addStudent(directory):
 
 
 def removeStudent(directory, student):
-    """
-        Function Name: removeStudent
-        Parameters:
-            Directory <dict> : Student Directory that is specified in the main() function
-            student <String> : String that corresponds to the student name
-            Return Type:  none
-        Description:
-            procedure that removes the student at directory[student]
-    """
-    pass
-
+    directory.pop(student)
 def updateGrade(directory, student):
-    """
-     Function Name: updateGrades
-        Parameters:
-            Directory <dict> : Student Directory that is specified in the main() function
-            student <String> : String that corresponds to the student name
-            Return Type:  none
-        Description:
-            procedure that updates a student's gradebook
-    """
-    pass
-
+    for grade in directory[student]["grades"]:
+        directory[student]["grades"][grade] = int(input(f"what is the new grade for {grade}?"))
+        
+    
 
 def calculateGPA(directory, student):
-    """
-     Function Name: calculateGPA
-        Parameters:
-            Directory <dict> : Student Directory that is specified in the main() function
-            student <String> : String that corresponds to the student name
-            Return Type:  <float> average of all grades
-        Description:
-            creates a GPA variable set equal to zero, then computes the average (mean) of all of the grades in the gradebook
-    """
     GPA = 0
-    pass
+    for grades in directory[student]["grades"]:
+        GPA += directory[student]["grades"][grades]
+        
+    GPA/= len(directory[student]["grades"])
+    return print(f"{GPA} is the average of all grades")
 
 
 def checkHonorRoll(directory,student):
@@ -108,6 +61,10 @@ def checkHonorRoll(directory,student):
         Description:
             Calls the calculateGPA() subroutine that gets the GPA then checks all grades in the grade book to see if they are all over 81, then returns True or False depending on if the GPA is 88 or better
     """
+    GPA = calculateGPA(directory,student)
+    for grades in directory[student]["grades"][grades]:
+        if grades <= 81:
+            directory[student]["calculateGPA"]
     pass
 
 def printMenu():
